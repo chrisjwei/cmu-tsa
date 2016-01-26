81a5e56a530fbda5a03d225ce1321c926aafb3aa
Chris Wei <cwei1994@gmail.com>
2016-01-25 23:22:29 -0500

    starting fresh :)

--- vendor/extensions/banner_images/spec/support/factories/refinery/banner_images.rb
+++
@@ -1,7 +0,0 @@
-
-FactoryGirl.define do
-  factory :banner_image, :class => Refinery::BannerImages::BannerImage do
-    sequence(:name) { |n| "refinery#{n}" }
-  end
-end
-
--- vendor/extensions/officers/spec/support/factories/refinery/officers.rb
+++
@@ -1,7 +0,0 @@
-
-FactoryGirl.define do
-  factory :officer, :class => Refinery::Officers::Officer do
-    sequence(:name) { |n| "refinery#{n}" }
-  end
-end
-
